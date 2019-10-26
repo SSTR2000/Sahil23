8c072e50f1af64f2083500b028033dd7dc3d2d4f
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-10-26 21:20:05 +0530

    codded

---
+++ cmake-build-debug/CMakeFiles/Sahil23.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sahil23.dir/main.c.obj"
+  "Sahil23.exe"
+  "Sahil23.exe.manifest"
+  "Sahil23.pdb"
+  "libSahil23.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sahil23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
